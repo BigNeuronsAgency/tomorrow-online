5f06e4fd7acf1d3dd4b0c3c826e95c148c408cc2
Tomorrow Online <hello@tomorrow.online>
2026-01-29 18:30:59 +0100

    fix: Cloudinary preset 'tomorrow-briefs' (unsigned)

--- js/submit-netlify.js
+++ js/submit-netlify.js
@@ -18,7 +18,7 @@ window.submitForm = async function() {
       try {
         const formData = new FormData();
         formData.append('file', file);
-        formData.append('upload_preset', 'ml_default'); // Cloudinary unsigned preset
+        formData.append('upload_preset', 'tomorrow-briefs');
         formData.append('folder', 'tomorrow-briefs');
         
         const response = await fetch('https://api.cloudinary.com/v1_1/dzafqfsbo/auto/upload', {
